d81b461dcb62763f79dd0f23698d9744c1d0c747
Sergey Komar <komarserjio@gmail.com>
2013-02-27 20:03:11 +0200

    Signup view/form improvements

--- django/notejam/users/forms.py
+++ django/notejam/users/forms.py
@@ -1,10 +1,17 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
-class SignupForm(forms.Form):
+class SignupForm(forms.ModelForm):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput())
     repeat_password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+
+    def save(self, *args, **kwargs):
+        pass
 
 
 class SigninForm(forms.Form):
--- django/notejam/users/views.py
+++ django/notejam/users/views.py
@@ -2,10 +2,19 @@ from django.contrib.auth.models import User
 from django.views.generic.edit import FormView
 from django.views.generic.edit import CreateView
 
+from users.forms import SignupForm
+
 
 class SignupView(CreateView):
     model = User
-    pass
+    form_class = SignupForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        pass
+
+    def form_invalid():
+        pass
 
 
 class SigninView(FormView):
