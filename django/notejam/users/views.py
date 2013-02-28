121d38a8fc22e24ef46e36470fcdccfd3f0c10bf
Sergey Komar <komarserjio@gmail.com>
2013-02-28 17:12:33 +0200

    Added basic signin functionality

--- django/notejam/users/views.py
+++ django/notejam/users/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import redirect
 from django.views.generic.edit import FormView
 from django.views.generic.edit import CreateView
 
@@ -18,10 +20,25 @@ class SigninView(FormView):
     form_class = SigninForm
     success_url = reverse_lazy('home')
 
-    def form_valid(self, form):
-        import pdb
-        pdb.set_trace()
-        pass
+    def post(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        if form.is_valid():
+            user = authenticate(
+                email=form.cleaned_data['email'],
+                password=form.cleaned_data['password']
+            )
+            if user is not None:
+                login(request, user)
+                return redirect(reverse_lazy('home'))
+            else:
+                fail_message = "Wrong email or password"
+
+            return self.render_to_response(
+                self.get_context_data(form=form, fail_message=fail_message)
+            )
+        else:
+            return self.form_invalid(form, **kwargs)
 
 
 class ForgotPasswordView(FormView):
