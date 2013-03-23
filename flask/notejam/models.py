eeeb2f081cbc2a9283b9ea0c8d08836121e2c01f
Sergey Komar <komarserjio@gmail.com>
2013-03-24 01:14:51 +0200

    Flask: Signin/out implementation

--- flask/notejam/models.py
+++ flask/notejam/models.py
@@ -1,13 +1,20 @@
 from werkzeug.security import (generate_password_hash,
-    check_password_hash)
+check_password_hash)
+from flask.ext.login import UserMixin
 
 from notejam import db
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(36))
+
+    @staticmethod
+    def authenticate(email, password):
+        user = User.query.filter_by(email=email).first()
+        if user and user.check_password(password):
+            return user
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
--- flask/notejam/views.py
+++ flask/notejam/views.py
@@ -1,28 +1,50 @@
 from flask import render_template, flash, request, redirect, url_for
+from flask.ext.login import login_user, login_required, logout_user
 
-from notejam import app, db
+from notejam import app, db, login_manager
 from notejam.forms import SigninForm, SignupForm
 from notejam.models import User
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
 @app.route('/')
+@login_required
 def index():
     return render_template('base.html')
 
 
+# @TODO use macro for form fields in template
 @app.route('/signin/', methods=['GET', 'POST'])
 def signin():
     form = SigninForm()
-    if request.method == 'POST' and form.validate():
-        flash('OK', 'success')
+    if form.validate_on_submit():
+        auth_user = User.authenticate(form.email.data, form.password.data)
+        if auth_user:
+            login_user(auth_user)
+            flash('You are signed in!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Wrong credentials.', 'error')
     return render_template('users/signin.html', form=form)
 
 
+@app.route('/signout/')
+def signout():
+    logout_user()
+    return redirect(url_for('signin'))
+
+
+# @TODO use macro for form fields in template
 @app.route('/signup/', methods=['GET', 'POST'])
 def signup():
     form = SignupForm()
     if request.method == 'POST' and form.validate():
         user = User(email=form.email.data)
+        user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
         flash('Account is created. Now you can sign in.', 'success')
