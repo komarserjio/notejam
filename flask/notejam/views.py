f5b770b965d84934798e1562439010f7b7edd8dd
Sergey Komar <komarserjio@gmail.com>
2013-03-26 00:05:41 +0200

    Flask: List ordering

--- flask/notejam/views.py
+++ flask/notejam/views.py
@@ -17,7 +17,10 @@ def load_user(user_id):
 @app.route('/')
 @login_required
 def index():
-    notes = Note.query.filter_by(user=current_user).all()
+    notes = (Note.query
+                 .filter_by(user=current_user)
+                 .order_by(_get_order_by(request.args.get('order')))
+                 .all())
     return render_template('notes/list.html', notes=notes)
 
 
@@ -115,7 +118,11 @@ def update_pad(pad_id):
 @login_required
 def pad_notes(pad_id):
     pad = _get_user_object_or_404(Pad, pad_id, current_user)
-    return render_template('pads/note_list.html', pad=pad)
+    notes = (Note.query
+                 .filter_by(user=current_user, pad=pad)
+                 .order_by(_get_order_by(request.args.get('order')))
+                 .all())
+    return render_template('pads/note_list.html', pad=pad, notes=notes)
 
 
 @app.route('/pads/<int:pad_id>/delete/', methods=['GET', 'POST'])
@@ -206,3 +213,13 @@ def _get_user_object_or_404(model, object_id, user, code=404):
     ''' get an object by id and owner user or raise an abort '''
     result = model.query.filter_by(id=object_id, user=user).first()
     return result or abort(code)
+
+
+def _get_order_by(param='-updated_at'):
+    ''' get model order param by string description '''
+    return {
+        'name': Note.name.asc(),
+        '-name': Note.name.desc(),
+        'updated_at': Note.updated_at.asc(),
+        '-updated_at': Note.updated_at.desc(),
+    }.get(param, Note.updated_at.desc())
