d5da90d25f9cc86f6facbba66d4d2f5c93198ee3
Sergey Komar <komarserjio@gmail.com>
2013-03-30 11:01:21 +0200

    Pyramid: Fixed base template anchor issue

--- pyramid/notejam/notejam/views.py
+++ pyramid/notejam/notejam/views.py
@@ -9,7 +9,7 @@ from .models import (
     )
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='templates/base.pt')
 def my_view(request):
     try:
         one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
