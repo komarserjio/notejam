cda1cb3d772a8f3a16893b973f1113bb8951d143
Sergey Komar <komarserjio@gmail.com>
2013-04-03 21:26:13 +0300

    Pyramid: Templates experiments

--- pyramid/notejam/notejam/__init__.py
+++ pyramid/notejam/notejam/__init__.py
@@ -16,5 +16,6 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('signin', '/signin/')
     config.scan()
     return config.make_wsgi_app()
--- pyramid/notejam/notejam/views.py
+++ pyramid/notejam/notejam/views.py
@@ -14,28 +14,13 @@ def my_view(request):
     try:
         one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
     except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
+        return Response('text', content_type='text/plain', status_int=500)
     return {'one': one, 'project': 'notejam'}
 
-conn_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
 
-1.  You may need to run the "initialize_notejam_db" script
-    to initialize your database tables.  Check your virtual 
-    environment's "bin" directory for this script and try to run it.
-
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
-
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
-
-
+@view_config(route_name='signin', renderer='templates/signin.pt')
 def signin(request):
-    pass
+    return {}
 
 
 def signup(request):
