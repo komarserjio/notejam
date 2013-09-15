fbd5c6f17f4b3a04a5ffa5af675accef098cefd8
Sergey Komar <komarserjio@gmail.com>
2013-09-15 16:08:14 +0300

    Django: Signin test case

--- django/notejam/users/tests.py
+++ django/notejam/users/tests.py
@@ -22,19 +22,19 @@ class SignUpTest(TestCase):
     def test_signup_fail_required_fields(self):
         response = self.client.post(reverse('signup'), {})
         self.assertEqual(
-            response.context_data['form'].errors.keys(),
+            response.context['form'].errors.keys(),
             self._get_user_data().keys()
         )
 
     def test_signup_fail_invalid_email(self):
         invalid_data = self._get_user_data(email='invalid email')
         response = self.client.post(reverse('signup'), invalid_data)
-        self.assertIn('email', response.context_data['form'].errors)
+        self.assertIn('email', response.context['form'].errors)
 
     def test_signup_fail_passwords_dont_match(self):
         invalid_data = self._get_user_data(password='another pass')
         response = self.client.post(reverse('signup'), invalid_data)
-        self.assertIn('repeat_password', response.context_data['form'].errors)
+        self.assertIn('repeat_password', response.context['form'].errors)
 
     def test_signup_fail_email_exists(self):
         data = self._get_user_data()
@@ -45,4 +45,39 @@ class SignUpTest(TestCase):
         )
 
         response = self.client.post(reverse('signup'), data)
-        self.assertIn('email', response.context_data['form'].errors)
+        self.assertIn('email', response.context['form'].errors)
+
+
+class SignInTest(TestCase):
+    def _get_user_data(self, **kwargs):
+        user_data = {
+            'email': 'email@example.com',
+            'password': 'secure_password'
+        }
+        user_data.update(**kwargs)
+        return user_data
+
+    def test_signin_success(self):
+        data = self._get_user_data()
+        user = User.objects.create(email=data['email'], username=data['email'])
+        user.set_password(data['password'])
+        user.save()
+
+        response = self.client.post(reverse('signin'), data)
+        self.assertRedirects(response, reverse('home'))
+
+    def test_signin_fail(self):
+        response = self.client.post(reverse('signin'), self._get_user_data())
+        self.assertContains(response, 'Wrong password or email')
+
+    def test_signin_fail_required_fields(self):
+        response = self.client.post(reverse('signin'), {})
+        self.assertEqual(
+            response.context['form'].errors.keys(),
+            self._get_user_data().keys()
+        )
+
+    def test_signin_fail_invalid_email(self):
+        invalid_data = self._get_user_data(email='invalid email')
+        response = self.client.post(reverse('signin'), invalid_data)
+        self.assertIn('email', response.context['form'].errors)
