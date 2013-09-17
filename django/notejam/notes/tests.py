82a8764690498176ceb7e5aed1549bb27ebcc7e5
Sergey Komar <komarserjio@gmail.com>
2013-09-17 19:35:24 +0300

    Django: Improved notes tests

--- django/notejam/notes/tests.py
+++ django/notejam/notes/tests.py
@@ -1,16 +1,25 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+from django.core.urlresolvers import reverse
+from django.test import TestCase, Client
 
-Replace this with more appropriate tests for your application.
-"""
-
-from django.test import TestCase
+from notejam.tests import create_user
+from notes.models import Note
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class NoteTest(TestCase):
+    def setUp(self):
+        user_data = {
+            'email': 'user@example.com',
+            'password': 'secure_password'
+        }
+        self.user = create_user(user_data)
+        self.client.login(**user_data)
+
+    def test_create_success(self):
+        self.client.post(
+            reverse('create_note'), {'name': 'pad', 'text': 'pad text'})
+        self.assertEquals(1, Note.objects.count())
+
+    def test_create_fail_required_name(self):
+        response = self.client.post(reverse('create_note'), {})
+        self.assertEquals(
+            set(['name', 'text']), set(response.context['form'].errors.keys()))
