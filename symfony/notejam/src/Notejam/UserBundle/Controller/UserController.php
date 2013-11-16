0a34690257f1ce12ac17ea2882fbd81d09477ac8
Sergey Komar <komarserjio@gmail.com>
2013-11-16 22:51:23 +0200

    Symfony: Sign Up/In functionality (almost works now)

--- symfony/notejam/src/Notejam/UserBundle/Controller/UserController.php
+++ symfony/notejam/src/Notejam/UserBundle/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace Notejam\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
 use Notejam\UserBundle\Entity\User;
 use Notejam\UserBundle\Form\Type\UserType;
 
@@ -17,23 +18,28 @@ class UserController extends Controller
 
     public function signupAction(Request $request)
     {
-        $user = new User();
-        $userType = new UserType();
-        $form = $this->createForm(new UserType(), $user);
+        $form = $this->createForm(new UserType(), new User());
 
         if ($request->getMethod() == 'POST') {
             $em = $this->getDoctrine()->getEntityManager();
             $form->handleRequest($request);
             if ($form->isValid()) {
                 $user = $form->getData();
+
                 $factory = $this->get('security.encoder_factory');
                 $encoder = $factory->getEncoder($user);
-                $password = $encoder->encodePassword('123123', $user->getSalt());
+                $password = $encoder->encodePassword(
+                    $user->getPassword(), $user->getSalt());
                 $user->setPassword($password);
+
                 $em->persist($user);
                 $em->flush();
 
-                // flash message
+                $this->get('session')->getFlashBag()->add(
+                    'success',
+                    'Now you can sign in'
+                );
+                return $this->redirect($this->generateUrl('signin'));
             }
         }
         return $this->render(
@@ -44,7 +50,34 @@ class UserController extends Controller
 
     public function signinAction()
     {
-        // code...
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        $user = new User();
+        $form = $this->createFormBuilder(new User())
+                     ->setAction($this->generateUrl('login_check'))
+                     ->add('email', 'text')
+                     ->add('password', 'text')
+                     ->add('save', 'submit')
+                     ->getForm();
+ 
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContext::AUTHENTICATION_ERROR
+            );
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+        }
+        if ($error) {
+            $this->get('session')->getFlashBag()->add(
+                'error',
+                $error->getMessage()
+            );
+        }
+ 
+        return $this->render(
+            'NotejamUserBundle:User:signin.html.twig', 
+            array('form' => $form->createView())
+        );
     }
 
     public function forgotPasswordAction()
