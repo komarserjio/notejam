d6e8af6aa4e717c737a3a8b5a28e9a9029bc5bd3
Sergey Komar <komarserjio@gmail.com>
2014-01-08 20:55:46 +0100

    Symfony: Added user tests class skeleton

---
+++ symfony/notejam/src/Notejam/UserBundle/Tests/Controller/UserControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Notejam\NoteBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UserControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+    }
+
+    public function testSignupSuccess() 
+    {
+    }
+
+    public function testSignupErrorInvalidEmail() 
+    {
+    }
+
+    public function testSignupErrorPasswords() 
+    {
+    }
+
+    public function testSignupErrorRequiredFields() 
+    {
+    }
+
+    public function testSigninSuccess() 
+    {
+    }
+
+    public function testSigninFail() 
+    {
+    }
+
+    public function testSigninErrorRequiredFields() 
+    {
+    }
+}
+
