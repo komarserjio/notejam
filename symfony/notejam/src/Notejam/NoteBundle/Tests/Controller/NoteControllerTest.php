c9d390801c102c6d5cdc99e17d05f3085a62318d
Sergey Komar <komarserjio@gmail.com>
2014-01-14 21:42:01 +0100

    Symfony: Note tests definitions

--- symfony/notejam/src/Notejam/NoteBundle/Tests/Controller/NoteControllerTest.php
+++ symfony/notejam/src/Notejam/NoteBundle/Tests/Controller/NoteControllerTest.php
@@ -6,7 +6,31 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class NoteControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testCreateNoteSuccess()
+    {
+    }
+
+    public function testCreateNoteFailRequiredFields() 
+    {
+    }
+
+    public function testEditNoteSuccess() 
+    {
+    }
+
+    public function testEditNoteFailRequiredFields() 
+    {
+    }
+
+    public function testDeleteNoteSuccess()
+    {
+    }
+
+    public function testDeleteNoteFailAnotherUser()
+    {
+    }
+
+    public function testViewNoteSuccess()
     {
     }
 }
