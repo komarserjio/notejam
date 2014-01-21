726eaf36c809fd9c73313fd1f30be0c896cb9852
Sergey Komar <komarserjio@gmail.com>
2014-01-21 22:22:10 +0100

    Symfony: First successfull test

--- symfony/notejam/src/Notejam/UserBundle/Tests/Controller/UserControllerTest.php
+++ symfony/notejam/src/Notejam/UserBundle/Tests/Controller/UserControllerTest.php
@@ -1,20 +1,26 @@
 <?php
-
 namespace Notejam\NoteBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+
 
 class UserControllerTest extends WebTestCase
 {
+    public function setUp() {
+        $this->loadFixtures(array());
+    }
+
     public function testSignupSuccess() 
     {
         $client = static::createClient();
+        $this->loadFixtures(array());
         $crawler = $client->request('GET', '/signup');
         $form = $crawler->filter('button')->form();
-        $form['user[email]'] = 'test@example.com';
+        $form['user[email]'] = 'test1@example.com';
         $form['user[password][password]'] = 'test@example.com';
         $form['user[password][confirm]'] = 'test@example.com';
         $crawler = $client->submit($form);
+        $this->assertTrue($client->getResponse()->isRedirect());
     }
 
     public function testSignupFailInvalidEmail() 
@@ -45,4 +51,3 @@ class UserControllerTest extends WebTestCase
     {
     }
 }
-
