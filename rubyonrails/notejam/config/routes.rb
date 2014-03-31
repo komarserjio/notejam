f8da213af1e08d43de97a0204bd1cf2df27b689c
Sergey Komar <komarserjio@gmail.com>
2014-03-31 21:34:07 +0200

    Rubyonrails: Configured routes.rb

--- rubyonrails/notejam/config/routes.rb
+++ rubyonrails/notejam/config/routes.rb
@@ -1,30 +1,42 @@
 Notejam::Application.routes.draw do
   # You can have the root of your site routed with "root"
-  root 'note#list'
+  root 'note#list', as: :all_notes
 
+  # Pad urls
   get "pads/:id/edit" => "pad#edit", as: :edit_pad
   post "pads/:id/edit" => "pad#edit"
   
-  get "pad/:id/delete" => "pad#delete", as: :delete_pad
-  post "pad/:id/delete" => "pad#delete"
+  get "pads/:id/delete" => "pad#delete", as: :delete_pad
+  post "pads/:id/delete" => "pad#delete"
+
+  get "pads/:id/" => "pad#view", as: :view_pad_notes
 
   get "pads/create" => "pad#create", as: :create_pad
   post "pads/create" => "pad#create"
 
+  # Note urls
   get "notes/create" => "note#create", as: :create_note
   post "notes/create" => "note#create"
 
   get "notes/:id/edit" => "note#edit", as: :edit_note
-  post "notes/:id/edit"
+  post "notes/:id/edit" => "notes#edit"
 
-  get "notes/:id/delete" => "note#delete", as: :delete
-  post "notes/:id/delete"
+  get "notes/:id/delete" => "note#delete", as: :delete_note
+  post "notes/:id/delete" => "note#delete"
 
-  get "note/edit"
-  get "note/delete"
-  get "note/create"
-  get "note/view"
-  get "user/signup"
+  get "notes/:id/" => "note#view", as: :view_note
+
+  # User urls
+  get "signup/" => "user#signup", as: :signup
+  post "signup/" => "user#signup"
+
+  get "signin/" => "user#signin", as: :signin
+  post "signin/" => "user#signin"
+
+  get "signout/" => "user#signout", as: :signout
+
+  get "forgot-password/" => "user#forgot_password", as: :forgot_password
+  post "forgot-password/" => "user#forgot_password"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
