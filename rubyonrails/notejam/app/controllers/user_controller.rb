6b46694ca8ab9c51539b6f06c8113083f5ebe0cb
Sergey Komar <komarserjio@gmail.com>
2014-04-13 00:16:50 +0200

    Rubyonrails: Added signin form and flash messages support

--- rubyonrails/notejam/app/controllers/user_controller.rb
+++ rubyonrails/notejam/app/controllers/user_controller.rb
@@ -1,15 +1,22 @@
 class UserController < ApplicationController
+  #before_filter :authenticate_user, :only => [:signin]
   def signup
     if params[:user]
       @user = User.new(user_params)
       if @user.valid?
         @user.save
-        redirect_to url_for :signin
+        redirect_to(
+          url_for(:signin), 
+          :flash => {:success => "Now you can sign in"}
+        )
       end
     end
   end
 
   def signin
+    if request.post?
+      
+    end
   end
 
   def forgot_password
