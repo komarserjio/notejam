16322d94333f810b4ab6b28a74c967f3e913867d
Sergey Komar <komarserjio@gmail.com>
2014-05-03 23:56:44 +0200

    Rubyonrails: Added user signup tests

--- rubyonrails/notejam/test/controllers/user_controller_test.rb
+++ rubyonrails/notejam/test/controllers/user_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UserControllerTest < ActionController::TestCase
+
   test "should successfully signup" do
     assert_difference('User.count') do
       post :signup, user: {
@@ -9,5 +10,38 @@ class UserControllerTest < ActionController::TestCase
       }
     end
     assert_redirected_to signin_path
+  end
+
+  test "should not signup if invalid email" do
+    post :signup, user: {
+      email: 'invalid email', password: 'password',
+      password_confirmation: 'password'
+    }
+    assert_select ".errorlist li", "email is invalid"
+  end
+
+  test "should not signup if required fields are missing" do
+    post :signup, user: {
+      email: '', password: '',
+      password_confirmation: ''
+    }
+    assert_select ".errorlist li"
+  end
+
+  test "should not signup if email exists" do
+    email = users(:existent_user).email
+    post :signup, user: {
+      email: email, password: 'password',
+      password_confirmation: 'password'
+    }
+    assert_select ".errorlist li", "email has already been taken"
+  end
+
+  test "should not signup if passwords do not match" do
+    post :signup, user: {
+      email: 'user@example.com', password: 'password1',
+      password_confirmation: 'password2'
+    }
+    assert_select ".errorlist li"
   end
 end
