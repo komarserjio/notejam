b734b084363dfcbd4d85379f8e4bef3bc4804471
Sergey Komar <komarserjio@gmail.com>
2014-05-16 00:02:24 +0200

    Laravel: Improved error rendering

--- laravel/notejam/app/views/user/signup.blade.php
+++ laravel/notejam/app/views/user/signup.blade.php
@@ -1,13 +1,13 @@
 @extends('layout')
 @section('content')
-    {{ Form::open() }}
+    {{ Form::open(array('class' => 'offset-by-six')) }}
 
     {{ Form::label('email', 'E-mail') . Form::text('email', Input::old('email')) }}
-    {{ $errors->first('email'); }}
+    @include('partials.error', array('error' => $errors->first('email')))
     {{ Form::label('password', 'Password') . Form::password('password') }}
-    {{ $errors->first('password'); }}
+    @include('partials.error', array('error' => $errors->first('password')))
     {{ Form::label('password_confirmation', 'Confirm password') . Form::password('password_confirmation') }}
-    {{ $errors->first('confirm_password'); }}
+    @include('partials.error', array('error' => $errors->first('password_confirmation')))
 
     {{ Form::submit('Sign Up') }}
     <p><a href="#">Don't have an account?</a></p>
