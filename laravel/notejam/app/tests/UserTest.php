3aedef9ea35a329b7de555e214360268a3e82039
Sergey Komar <komarserjio@gmail.com>
2014-05-22 16:01:32 +0200

    Laravel: Added more user tests

--- laravel/notejam/app/tests/UserTest.php
+++ laravel/notejam/app/tests/UserTest.php
@@ -2,10 +2,13 @@
 
 class UserTest extends TestCase {
 
-    public function getUserData() {
-        return array(
-            'email' => 'user@example.com',
-            'password' => 'secure_password'
+    public function getUserData($override = array()) {
+        return array_merge(
+            array(
+                'email' => 'user@example.com',
+                'password' => 'secure_password'
+            ),
+            $override
         );
     }
 
@@ -13,7 +16,6 @@ class UserTest extends TestCase {
     {
         $data = $this->getUserData();
         $data['password_confirmation'] = $data['password'];
-        $data['_token'] = csrf_token();
         $crawler = $this->client->request(
             'POST', URL::route('signup'), $data
         );
@@ -23,22 +25,45 @@ class UserTest extends TestCase {
 
     public function testSignupFailRequiredFields()
     {
-        // code...
+        $crawler = $this->client->request(
+            'POST', URL::route('signup'), array()
+        );
+        $this->assertSessionHasErrors(
+            array('email', 'password', 'password_confirmation')
+        );
     }
 
     public function testSignupFailInvalidEmail()
     {
-        // code...
+        $data = $this->getUserData(array('email' => 'invalid'));
+        $crawler = $this->client->request(
+            'POST', URL::route('signup'), $data
+        );
+        $this->assertSessionHasErrors('email');
     }
 
     public function testSignupFailEmailExists()
     {
-        // code...
+        $email = 'exists@example.com';
+        $user = User::create(
+            array('email' => $email, 'password' => 'password')
+        );
+
+        $data = $this->getUserData(array('email' => $email));
+        $crawler = $this->client->request(
+            'POST', URL::route('signup'), $data
+        );
+        $this->assertSessionHasErrors('email');
     }
 
     public function testSignupFailPasswordsNotMatch()
     {
-        // code...
+        $data = $this->getUserData();
+        $data['password_confirmation'] = 'another_password';
+        $crawler = $this->client->request(
+            'POST', URL::route('signup'), $data
+        );
+        $this->assertSessionHasErrors('password');
     }
 }
 
