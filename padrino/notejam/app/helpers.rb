08a212b6b179f21e1bb50763bec49d3551f98229
Sergey Komar <komarserjio@gmail.com>
2014-05-25 22:56:56 +0200

    Padrion: Basic project structure

---
+++ padrino/notejam/app/helpers.rb
@@ -0,0 +1,13 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module Notejam
+  class App
+    module 
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers 
+  end
+end
---
+++ padrino/notejam/spec/spec_helper.rb
@@ -0,0 +1,20 @@
+RACK_ENV = 'test' unless defined?(RACK_ENV)
+require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
+
+RSpec.configure do |conf|
+  conf.include Rack::Test::Methods
+end
+
+# You can use this method to custom specify a Rack app
+# you want rack-test to invoke:
+#
+#   app Notejam::App
+#   app Notejam::App.tap { |a| }
+#   app(Notejam::App) do
+#     set :foo, :bar
+#   end
+#
+def app(app = nil, &blk)
+  @app ||= block_given? ? app.instance_eval(&blk) : app
+  @app ||= Padrino.application
+end
