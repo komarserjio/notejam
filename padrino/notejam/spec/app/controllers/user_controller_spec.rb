5833c3329d0a74a02642789ebe7e88a6196032fe
Sergey Komar <komarserjio@gmail.com>
2014-06-06 23:00:50 +0200

    Padrino: User test cases structure

--- padrino/notejam/spec/app/controllers/user_controller_spec.rb
+++ padrino/notejam/spec/app/controllers/user_controller_spec.rb
@@ -8,4 +8,19 @@ describe "UserController" do
   it "returns hello world" do
     last_response.body.should == "Hello World"
   end
+
+  it "successfully signs up" do
+  end
+
+  it "requires fields to sign up" do
+  end
+
+  it "doesn't sign up if email is taken" do
+  end
+
+  it "successfully signs in" do
+  end
+
+  it "doesn't sign in if invalid credentials" do
+  end
 end
