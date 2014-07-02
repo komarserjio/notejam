2e942a6b3d47f7da2d3b25f98a781549ecf94262
Sergey Komar <komarserjio@gmail.com>
2014-07-02 16:15:09 +0200

    Padrino: First pad test added

--- padrino/notejam/spec/app/controllers/pad_controller_spec.rb
+++ padrino/notejam/spec/app/controllers/pad_controller_spec.rb
@@ -1,12 +1,32 @@
 require 'spec_helper'
 
 describe "PadController" do
-  before do
-    get "/"
+
+  def pad_data
+    {
+      "name" => "Pad"
+    }
   end
 
-  it "returns hello world" do
+  it "requires to be signed in to create pad" do
+    post "/pads/create", {
+      "pad" => pad_data
+    }
+    last_response.should be_redirect
+    follow_redirect!
+    last_request.url.should include("/signin")
+  end
+
+  it "creates pad successfully" do
     #last_response.body.should == "Hello World"
+    #post "/signup", {
+      #"user" => user_data
+    #}
+    #last_response.should be_redirect
+    #follow_redirect!
+    #last_request.url.should include("/signin")
+
+    #expect(User.count).to eq(1)
   end
 end
 
