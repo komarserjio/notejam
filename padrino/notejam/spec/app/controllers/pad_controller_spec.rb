3d232fc1ed88e021b4530abe36f29a22f8953616
Sergey Komar <komarserjio@gmail.com>
2014-07-14 23:43:12 +0200

    Padrino: Finalized pad tests

--- padrino/notejam/spec/app/controllers/pad_controller_spec.rb
+++ padrino/notejam/spec/app/controllers/pad_controller_spec.rb
@@ -65,6 +65,76 @@ describe "PadController" do
     }
     expect(Pad.get(pad.id).name).to eq(pad_data['name'])
   end
+
+  it "requires mandatory fields to edit pad" do
+    user = User.create(user_data)
+    pad = Pad.create({name: "Initial name", user: user})
+    login_user user_data
+    post "/pads/#{pad.id}/edit", {
+      "pad" => {
+        "name" => ""
+      }
+    }
+    last_response.body.should include("Name must not be blank")
+  end
+
+  it "doens't allow to edit pad by not an owner" do
+    owner = User.create(user_data)
+    pad = Pad.create({name: "Initial name", user: owner})
+
+    data = user_data
+    data['email'] = 'another_user@example.com'
+    another_user = User.create(data)
+
+    login_user data
+    post "/pads/#{pad.id}/edit", {
+      "pad" => pad_data
+    }
+    expect(last_response.status).to eq(404)
+  end
+
+  it "views pad successfully" do
+    user = User.create(user_data)
+    pad = Pad.create({name: "Pad", user: user})
+    login_user user_data
+
+    get "/pads/#{pad.id}"
+    expect(last_response.status).to eq(200)
+  end
+
+  it "doesn't allow to view pad by not an owner" do
+    owner = User.create(user_data)
+    pad = Pad.create({name: "Pad", user: owner})
+
+    data = user_data
+    data['email'] = 'another_user@example.com'
+    another_user = User.create(data)
+
+    login_user data
+    get "/pads/#{pad.id}"
+    expect(last_response.status).to eq(404)
+  end
+
+  it "deletes pad successfully" do
+    user = User.create(user_data)
+    pad = Pad.create({name: "Pad", user: user})
+    login_user user_data
+    post "/pads/#{pad.id}/delete"
+    expect(Pad.count).to eq(0)
+  end
+
+  it "doesn't allow to delete pad by not an owner" do
+    owner = User.create(user_data)
+    pad = Pad.create({name: "Pad", user: owner})
+
+    data = user_data
+    data['email'] = 'another_user@example.com'
+    another_user = User.create(data)
+
+    login_user data
+    post "/pads/#{pad.id}/delete"
+    expect(last_response.status).to eq(404)
+  end
 end
 
 
