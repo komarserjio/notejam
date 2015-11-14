f42aef37de1a0a2bfd2f106d1139b0228af1224c
Filip Procházka <filip@prochazka.su>
2015-11-14 11:57:49 +0100

    Nette: cleanup the structure

--- nette/notejam/app/presenters/Error4xxPresenter.php
+++ nette/notejam/app/presenters/Error4xxPresenter.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Presenters;
+namespace Notejam\Presenters;
 
 use Nette;
 
--- nette/notejam/src/Notejam/Http/RouterFactory.php
+++ nette/notejam/src/Notejam/Http/RouterFactory.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App;
+namespace Notejam\Http;
 
 use Nette;
 use Nette\Application\Routers\RouteList;
 use Nette\Application\Routers\Route;
+
 
 
 class RouterFactory
@@ -16,7 +17,7 @@ class RouterFactory
 	public static function createRouter()
 	{
 		$router = new RouteList;
-		$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+		$router[] = new Route('<presenter>[/<action>]', 'Homepage:default');
 		return $router;
 	}
 
