3ada486ccd26d4aa18eb236a4dad5598cb048b73
Markus Malkusch <markus@malkusch.de>
2015-11-21 12:02:56 +0100

    Spring: Enable asynchronous execution
    In order to process blocking or long running tasks
    (e.g. sending an email) the @Async annotation is enabled
    to execute code concurrently.
    
    See also: komarserjio/notejam#52

--- spring/src/main/java/net/notejam/spring/Application.java
+++ spring/src/main/java/net/notejam/spring/Application.java
@@ -1,9 +1,17 @@
 package net.notejam.spring;
 
+import java.util.concurrent.Executor;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
@@ -16,6 +24,29 @@ import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 @SpringBootApplication
 @EnableSpringConfigured
 public class Application {
+
+    @EnableAsync
+    @Configuration
+    public static class AsyncConfiguration implements AsyncConfigurer {
+
+        @Value("${async.queueCapacity}")
+        private int queueCapacity;
+        
+        @Override
+        public Executor getAsyncExecutor() {
+            ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+            executor.setMaxPoolSize(Runtime.getRuntime().availableProcessors());
+            executor.setQueueCapacity(queueCapacity);
+            executor.initialize();
+            return executor;
+        }
+
+        @Override
+        public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+            return null;
+        }
+        
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
