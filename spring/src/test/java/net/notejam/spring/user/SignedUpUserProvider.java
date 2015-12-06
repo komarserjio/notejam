570a08b2795c58d2ae1d38440bb507ae9302eb42
Markus Malkusch <markus@malkusch.de>
2015-12-06 23:07:25 +0100

    Spring: Test sign in story
    See also: komarserjio/notejam#52

--- spring/src/test/java/net/notejam/spring/user/SignedUpUserProvider.java
+++ spring/src/test/java/net/notejam/spring/user/SignedUpUserProvider.java
@@ -6,31 +6,45 @@ import org.springframework.stereotype.Component;
 
 /**
  * A rule for providing a signed up a user.
- * 
+ *
  * The user will be deleted after each test.
- * 
+ *
  * @author markus@malkusch.de
  * @see <a href="bitcoin:1335STSwu9hST4vcMRppEPgENMHD2r1REK">Donations</a>
  */
 @Component
 public class SignedUpUserProvider extends ExternalResource {
 
+    /**
+     * The user service.
+     */
     @Autowired
     private UserService service;
 
+    /**
+     * The user repository.
+     */
     @Autowired
     private UserRepository repository;
 
+    /**
+     * The signed up user.
+     */
     private User user;
 
     /**
      * The email address of the provided user.
      */
     public static final String EMAIL = "test@example.org";
+    
+    /**
+     * The password of the provided user.
+     */
+    public static final String PASSWORD = "password";
 
     @Override
     protected void before() {
-        user = service.signUp(EMAIL, "password");
+        user = service.signUp(EMAIL, PASSWORD);
     }
 
     /**
