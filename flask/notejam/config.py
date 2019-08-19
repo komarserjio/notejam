e9ad1e7c1b55949a25ff37b3c3ee3fe4ebaf1cab
Byron Kim <nocode99@gmail.com>
2019-08-19 00:43:07 -0400

    changes to db settings

--- flask/notejam/config.py
+++ flask/notejam/config.py
@@ -1,5 +1,9 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+DB_HOST = os.getenv('MYSQL_HOST', 'localhost:3306')
+DB_NAME = os.getenv('MYSQL_DATABASE', 'notejam')
+DB_USER = os.getenv('MYSQL_USER', 'nj')
+DB_PASSWORD = os.getenv('MYSQL_PASSWORD', 'password')
 
 class Config(object):
     DEBUG = False
@@ -7,7 +11,8 @@ class Config(object):
     SECRET_KEY = 'notejam-flask-secret-key'
     CSRF_ENABLED = True
     CSRF_SESSION_KEY = 'notejam-flask-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'notejam.db')
+    SQLALCHEMY_DATABASE_URI = "mysql://%s:%s@%s/%s" % (DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'notejam.db')
 
 
 class ProductionConfig(Config):
