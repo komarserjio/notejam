64129b75567b466bb215fef325e9d967e7107220
Celso Coutinho <celsocoutinho@mail.com>
2021-03-06 15:38:18 +0100

    Fix bash error

--- devops/scripts/tf_run.sh
+++ devops/scripts/tf_run.sh
@@ -11,7 +11,7 @@ terraform init -input=false -backend-config="environments/$environment/$environm
 terraform apply -input=false -auto-approve
 
 # Get the App Service name for the environment.
-$webAppName=$(terraform output appservice_name)
+webAppName=$(terraform output appservice_name | tr -d '"')
 
 # Write the webAppName variable to the pipeline.
-echo "##vso[task.setvariable variable=WebAppName;isOutput=true]$webAppName" | tr -d '"'
+echo "##vso[task.setvariable variable=WebAppName;isOutput=true]$webAppName"
