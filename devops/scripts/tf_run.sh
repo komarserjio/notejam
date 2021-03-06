654ff8927da8ef37fe58d93fdbfb67e1a9bbb79d
Celso Coutinho <celsocoutinho@mail.com>
2021-03-06 14:48:18 +0100

    Update the script that runs TF because it is not executed inline anymore

--- devops/scripts/tf_run.sh
+++ devops/scripts/tf_run.sh
@@ -1,13 +1,17 @@
 # Exit when any command returns a failure status.
 set -e
 
+# Read arguments
+$environment = $1
+
 # Initialize Terraform.
 terraform init -input=false -backend-config="environments/$environment/$environment.backend.tfvars"
+
 # Apply the Terraform plan.
 terraform apply -input=false -auto-approve
 
 # Get the App Service name for the environment.
-WebAppName=$(terraform output appservice_name)
+$webAppName=terraform output appservice_name
 
-# Write the WebAppName variable to the pipeline.
-echo "##vso[task.setvariable variable=WebAppName;isOutput=true]$WebAppName" | tr -d '"'
+# Write the webAppName variable to the pipeline.
+echo "##vso[task.setvariable variable=WebAppName;isOutput=true]$webAppName" | tr -d '"'
