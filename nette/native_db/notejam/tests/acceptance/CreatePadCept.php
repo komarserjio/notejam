7692fb45511233f8886a2cc062fc3b5d465e4f7a
Sacrament84 <voloshin07@gmail.com>
2021-10-03 14:03:11 +0600

    Delete nette directory

--- nette/native_db/notejam/tests/acceptance/CreatePadCept.php
+++
@@ -1,8 +0,0 @@
-<?php
-$I = new AcceptanceTester($scenario);
-$I->wantTo('see pad can be successfully created');
-$I->testLogin();
-$I->amOnPage('/pads/create');
-$I->fillField('Name', 'Example pad');
-$I->click('Save');
-$I->see('Example pad');
