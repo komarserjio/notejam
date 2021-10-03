6f687308559ab650997736d271e4c1c1d07732f2
Sacrament84 <voloshin07@gmail.com>
2021-10-03 14:35:32 +0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python /app/django/notejam/manage.py syncdb --noinput
 python /app/django/notejam/manage.py migrate --noinput
-python /app/django/notejam/manage.py runserver 0.0.0.0:8080
+python /app/django/notejam/manage.py runserver 0.0.0.0:8000
